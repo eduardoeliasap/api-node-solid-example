e84d25435d7c4c702b885ccedb6a11c5a8d68ee4
Eduardo Elias <eduardo.eliasap@gmail.com>
2024-11-21 15:06:30 -0300

    adding use-cases and design patterns repository

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,35 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { RegistryUseCase } from '@/use-cases/register'
+import { PrismaUserRepository } from '@/repositories/prisma/prisma-user-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const userRepository = new PrismaUserRepository()
+    const registerUseCase = new RegistryUseCase(userRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send(err.message)
+    }
+
+    throw err
+    return reply.status(500).send()
+  }
+
+  return reply.status(201).send()
+}
---
+++ src/use-cases/register.ts
@@ -0,0 +1,29 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegistryUseCase {
+  constructor(private userRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const password_hash = await hash(password, 6)
+
+    await this.userRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
